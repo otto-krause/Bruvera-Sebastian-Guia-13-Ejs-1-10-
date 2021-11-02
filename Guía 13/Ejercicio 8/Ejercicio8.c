fa1ce66f39a5b5395f152f9a0adb9a6060bcb5c1
sebastianbruvera <86363949+sebastianbruvera@users.noreply.github.com>
2021-11-02 12:00:49 -0300

    Add files via upload

---
+++ Guía 13/Ejercicio 8/Ejercicio8.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int fibonacci[11], I;
+	printf("Los primeros 10 lugares de la secuencia de Fibonacci son: ");
+	fibonacci[0]=0;
+	fibonacci[1]=1;
+	for(I=2; I<=11; I++)
+	{
+		fibonacci[I]=fibonacci[I-1]+fibonacci[I-2];
+	}
+	for(I=0; I<=10; I++)
+	{
+		printf("%d\t", fibonacci[I]);
+	}
+	return 0;
+}
+
---
+++ Guía 13/Ejercicio 9/Ejercicio9.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	double lista[11];
+	printf("Ingrese una lista de 11 numeros distintos de 0:\n");
+	for(int I=1; I<=11; I++)
+	{
+		scanf("%lf", &lista[I]);
+		if(lista[I]==0)
+		{
+			printf("0 no es valido, lo siento\n");
+			break;
+		}
+	}
+	printf("La suma entre el primer y ultimo numero es %g\n", lista[1]+lista[11]);
+	for(int I=1; I<=11; I++)
+	{
+		if()
+		{
+			
+		}
+		else if()
+		{
+			
+		}
+		else()
+		{
+			
+		}
+	}
+	return 0;
+}
+
