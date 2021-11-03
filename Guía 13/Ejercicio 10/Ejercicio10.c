93fc77422602beddf1336e64387809ec4bdb08f4
sebastianbruvera <86363949+sebastianbruvera@users.noreply.github.com>
2021-11-03 12:00:30 -0300

    Add files via upload

---
+++ Guía 13/Ejercicio 10/Ejercicio10.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	double vec[13];
+	int a=0;
+	printf("Ingrese 10 numeros:\n");
+	for(int I=3; I<=12; I++)
+	{
+		scanf("%lf", &vec[I]);
+	}
+	for(int I=3; I<=12; I++)
+	{
+		vec[0]+=vec[I];
+	}
+	for(int I=3; I<=12; I++)
+	{
+		vec[1]= (double) vec[1]*vec[I];
+	}
+	vec[2]= (double) vec[0]/10;
+	for(int I=3; I<=12; I++)
+	{
+		if(vec[I]<vec[2])
+		{
+			a+=1;
+		}
+	}
+	printf("La suma de todos los numeros es %g\nEl producto es %g\nEl promedio es %g\nLa cantidad de numeros debajo del promedio es de %d", vec[0], vec[1], vec[2], a);
+	return 0;
+}
+
---
+++ Guía 13/Ejercicio 9/Ejercicio9.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	double lista[11];
+	int	a=0;
+	printf("Ingrese una lista de 10 numeros distintos de 0:\n");
+	for(int I=1; I<=10; I++)
+	{
+		scanf("%lf", &lista[I]);
+		if(lista[I]==0)
+		{
+			printf("0 no es un numero valido, lo siento\n");
+			return 0;
+		}
+	}
+	printf("La suma entre el primer y ultimo numero es %g\n", lista[1]+lista[10]);
+	for(int I=1; I<10; I++)
+	{
+		if(lista[I]<lista[I+1])
+		{
+			a+=1;
+		}
+		else if(lista[I]>lista[I+1])
+		{
+			a-=1;
+		}
+	}
+	if(a==9)
+	{
+		printf("La lista esta en orden ascendente\n");
+	}
+	else if(a==-9)
+	{
+		printf("La lista esta en orden descendente\n");
+	}
+	else
+	{
+		printf("La lista esta desordenada\n");
+	}
+	return 0;
+}
+
